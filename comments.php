47e785fd28487e91be167d6a03fd0cfbb97845b6
luminous0989 <56917276+luminous0989@users.noreply.github.com>
2022-02-22 17:45:52 +0800

    comments

--- comments.php
+++ comments.php
@@ -11,7 +11,7 @@
                                     if( ! have_comments()){
                                         echo "Leave a comment below";
                                     }else{
-                                        echo get_comments_count(). "Comments";
+                                        echo get_comments_number(). "Comments";
                                     }
                                 ?>
                             </h2>
@@ -20,11 +20,12 @@
 
 						<div class="comments-inner">
                                     <?php
-                                        wp_list_comments{
+                                        wp_list_comments(
                                             array(
-                                                
-                                            );
-                                        }
+                                            'avatar_size' => 64,
+                                            'style' => 'div',
+                                            )
+                                        )
                                     ?>
 
                         </div><!-- comments-inner -->
@@ -32,10 +33,16 @@
 					</div><!-- comments -->
 
 					<hr class="" aria-hidden="true">
-					<div id="respond" class="comment-respond">
-						<h2 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow"
-									id="cancel-comment-reply-link" href="/?p=1#respond" style="display:none;">Cancel
-									reply</a></small></h2>
-					</div><!-- #respond -->
-
+					
+                    <?php
+                        if( comments_open() ){
+                            comment_form(
+                                array(
+                                    'class_form' => '',
+                                    'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+                                    'title_reply_after' => '</h2>'
+                                )
+                            );
+                        }
+                    ?>
 				</div>
